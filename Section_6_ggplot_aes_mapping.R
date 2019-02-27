ca56bc717fd861dcf83143310978b7df22ff9f5c
Jocelino Mendes da Silva Júnior <34030698+jocelinojr@users.noreply.github.com>
2019-02-27 16:27:25 -0300

    Add files via upload

--- Section_6_ggplot_aes_mapping.R
+++ Section_6_ggplot_aes_mapping.R
@@ -42,8 +42,22 @@ s <- ggplot(data=movies, aes(x=budget.millions))
 s + geom_histogram(binwidth = 10, aes(fill=genre), color="White")
 # adds a border
 
+#-------------------------------------------------
+# Statistical transformations
 
-s + geom_density()
+
+# Smooth 
+g <- ggplot(data = movies, aes(x=critic.rating, y=audience.rating, colour=genre))
+g + geom_point() + geom_smooth(fill=NA)
+
+# Box plots
+bp <- ggplot(data = movies, aes(x=genre, y=audience.rating, color=genre))
+bp + geom_boxplot(size=1.2)
+
+bp + geom_boxplot(aes(y=critic.rating))
+
+bp2 <- ggplot(data = movies, aes(x=genre, y=critic.rating, color=genre))
+bp2 + geom_boxplot()
 
 
 
